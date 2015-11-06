2be9dcc1fd1bdbdfbfb331ffbcdfdb733f828e33
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2015-11-06 23:25:42 +0100

    some modifications to make it work

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Patched [SimpleMathJax](https://www.mediawiki.org/wiki/Extension:SimpleMathJax)
 
 - install from https://github.com/alexeygrigorev/SimpleMathJax/archive/master.zip
 - add `require_once "$IP/extensions/SimpleMathJax/SimpleMathJax.php";` to `LocalSettings.php`
+
+
+Script 
+	
+	
+	mkdir -p extensions/SimpleMathJax/
+	cd extensions/SimpleMathJax/
+	wget https://github.com/alexeygrigorev/SimpleMathJax/archive/master.zip
+	unzip master.zip 
+	mv SimpleMathJax-master/SimpleMathJax.* .
+	rm -r SimpleMathJax-master/ master.zip
+	cd ../..
+	echo 'require_once "$IP/extensions/SimpleMathJax/SimpleMathJax.php";' >> LocalSettings.php
--- SimpleMathJax.class.php
+++ SimpleMathJax.class.php
@@ -9,13 +9,14 @@ class SimpleMathJax {
         $tex = str_replace('\>', '\;', $tex);
         $tex = str_replace('<', '\lt', $tex);
         $tex = str_replace('>', '\gt', $tex);
-        return array("<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType'=>'nowiki');
+        return array("<span class='mathjax-wrapper'>[math]${tex}[/math]</span>", 'markerType' => 'nowiki');
     }
 
-    static function loadJS(&$out, &$skin ) {
-        global $wgSimpleMathJaxSize;
-        $snippet = file_get_contents('$IP/extensions/SimpleMathJax/snippet.html';, false);
+    static function loadJS(&$out, &$skin) {
+        $dir = dirname(__FILE__) . '/';
+        $snippet = file_get_contents($dir . 'snippet.html', false);
         $out->addScript($snippet);
         return true;
     }
 }
+?>
